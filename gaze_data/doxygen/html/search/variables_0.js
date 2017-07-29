7991d8436e40ee9636748345c980174df2e4e2e1
Rainer Heinelt <rainer.heinelt@t-online.de>
2017-07-29 22:17:46 +0200

    Ignoring doxygen output

--- gaze_data/doxygen/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['cbasehighlighter',['CBaseHighlighter',['../class_c_base_b_box.html#a7c5d97459f753e92bfa34497294683f4',1,'CBaseBBox']]]
-];
--- gaze_data/doxygen/html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['m_5fdheight',['m_dHeight',['../class_c_base_b_box.html#a24ab993e6da60fa3a1d67f21a2784e86',1,'CBaseBBox']]]
-];
