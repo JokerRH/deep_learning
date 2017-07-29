d7c5541e4babc51f61dfe60288dcf7da5d45ba9c
Rainer Heinelt <rainer.heinelt@t-online.de>
2017-07-29 22:01:16 +0200

    Added doxygen

---
+++ gaze_data/doxygen/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['m_5fdheight',['m_dHeight',['../class_c_base_b_box.html#a24ab993e6da60fa3a1d67f21a2784e86',1,'CBaseBBox']]]
+];
---
+++ gaze_data/doxygen/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cbasehighlighter',['CBaseHighlighter',['../class_c_base_b_box.html#a7c5d97459f753e92bfa34497294683f4',1,'CBaseBBox']]]
+];
